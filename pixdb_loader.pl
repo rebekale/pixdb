da6e84d304a33d317ed991165000f7d085cff858
Artin Rebekale <rebekale@gmail.com>
2014-02-22 16:44:24 -0800

    added flags for all variables

--- pixdb_loader.pl
+++ pixdb_loader.pl
@@ -1,31 +1,35 @@
+if (!$ARGV[3] ) {
+  usage();
+}
 use GD;
-$totalCols = 125;
-$h = 1400;
-$size = 9;
+$totalCols = $ARGV[2];
+$h = $ARGV[1];
+$size = $ARGV[0];
 $w = $size * $totalCols;
 
+$img = new GD::Image($w, $h,1);
 $img = new GD::Image($w, $h,1);
 
 $white = $img->colorAllocate(255,255,255);
     $black = $img->colorAllocate(0,0,0);       
     $red = $img->colorAllocate(255,0,0);      
     $blue = $img->colorAllocate(0,0,255);
-if (!$ARGV[0]) {
-  usage();
-}
         
 my $count;        
 $img->colorAllocate(255,255,255);
 $img->transparent($white);
 $img->interlaced('true');
-open(IN,"<$ARGV[0]");
-@colors;
-my $header = $img->colorAllocate($totalCols, $size,$h);
+open(IN,"<$ARGV[3]");
+$color;
+
+print "t\t $totalCols:".ord($totalCols).", s\t $size:".ord($size).", h\t $h:".ord($h)."\n";
+my $header = $img->colorAllocate(ord($totalCols), ord($size),ord($h));
     $img->setPixel(0,0,$header); 
+
 $row = 1;
 $flag = 0;  
 while(<IN>) {
-  print $_;
+  #print $_;
   chomp;
 
   next if length($_) > $size;
@@ -36,13 +40,13 @@ while(<IN>) {
     $o = ord($l);
     @rgb = (0,0,0);
     $rgb[int($col %3)] = $o;
-    print "( @rgb ) @ $col:$row\n";
+    #print "( @rgb ) @ $col:$row\n";
 
 
     #my $color = $img->colorAllocate(ord($l),0,0);
     #$color["$col,$row"] = $img->colorAllocate($rgb[0], $rgb[1],$rgb[2]);
-    $color["$col,$row"] = $img->colorAllocate($o,$o,$o);
-    $img->setPixel($col,$row,$color["$col,$row"]); 
+    $color->{"$col,$row"} = $img->colorAllocate($o,$o,$o);
+    $img->setPixel($col,$row,$color->{"$col,$row"}); 
     $col++
   } 
   $count++;
@@ -70,8 +74,9 @@ print "Total Records: $count\n";
 sub usage() {
 
   print "pixdb_loader.pl\n
-\tusage: perl pixdb_loader.pl wordlistfile\n
+\tusage: perl pixdb_loader.pl fieldsize maxrows maxcolumns wordlistfile\n
 \tworldlist file is a list of words delimited by cr/lf\n";
+print"\texample: perl pixdb_loader.pl 6 1000 10 wordlist.txt \n\t creates a file with 1000 rows and 10 columns of 6 character records\n";
 exit;         
 
 }
